f8ee4210403765a328a6830196283d490205a1eb
Guo <dominic@Guos-Mac-mini.lan>
2022-09-01 13:14:40 -0400

    but fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ function Card({ name, email, id }) {
     <div className='tc grow bg-light-yellow br3 pa3 ma2 dib bw2 shadow-5' >
       <img alt='robots' src={`https://robohash.org/${id}?set=set4`} width='200' height='200' />
       <div>
-        <h3>{name}</h3>
-        <p className=''>{email}</p>
+        <h3 className='gray'>{name}</h3>
+        <p className='gray underline'>{email}</p>
       </div>
     </div >
   );
