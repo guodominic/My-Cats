00ffd0509f60b8a4a6e026ae1bb0097d6579d78c
Guo <dominic@Guos-Mac-mini.lan>
2022-10-13 17:58:13 -0400

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ function Card({ name, email, id }) {
     <div
       onClick={() => { toCatPage(id) }}
       className='tc grow bg-yellow br3 pa3 ma2 dib bw2 shadow-5 pointer'>
-      <img alt='cats' src={`https://robohash.org/${id + 200}?set=set4`} width='200' height='200' />
+      <img alt='cats' src={`https://robohash.org/${id}?set=set4`} width='200' height='200' />
       <div>
         <h3 className='gray'>{name}</h3>
         <p className='gray underline'>{email}</p>
